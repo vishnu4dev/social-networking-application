95a83e666f0e538fec6a28932533b484577518ab
vishnu4dev <2vishnukkumar@gmail.com>
2020-08-02 10:32:39 +0530

    feat: getUserProfile -getByUserId

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,19 +1,33 @@
-import { check, validationResult } from 'express-validator';
+import { check, validationResult, body } from 'express-validator';
 
-
-export default class Validator {
-
-
-    async ProfileValidator(req,res,nex){
-        try {
-           return [ body('status', 'status is required').not().isEmpty(),
-            body('skills', 'Please enter list of skills').isEmail(),
-            body('name', 'Profile Name').exists()]
-        } catch (error) {
-            res.status(400).json({ errors: errors.array() })
-            console.log("Error in Validation middleware");
-        }
+const validate=()=>{
+    return{
+        "profile":[
+            check('status', 'status is required').not().isEmpty(),
+            body('skill').exists().withMessage('Skill list missing '),
+            check('name', 'Profile Name').exists(),
+            body('address').exists().withMessage('Address is req'),
+            body('phoneNumber').exists().withMessage("Phone number is req"),       
+        ],
+        "register":[
+            body('name').exists().withMessage('Name is req'), 
+            body('email').isEmail().withMessage('Email is Invalid'), 
+            body('password').isLength({ min: 6 }).withMessage('Password is invalid'), 
+        ]
     }
-
 }
 
+const RequestValidation=(req,res,next)=>{
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+    console.log(error,"******* Input Error *******");
+    res.status(422).send({error});
+    }
+    next();
+}
+
+
+module.exports={
+    validate,
+    RequestValidation,
+}
